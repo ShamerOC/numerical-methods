e49ce6c9c0dd5278ee94b64dbceb7c37a1ba298c
sh4 <7331319+ShamerOC@users.noreply.github.com>
2021-04-30 16:52:59 +0200

    scaled gauss passing tests

--- scaled gauss/source.cpp
+++ scaled gauss/source.cpp
@@ -24,40 +24,12 @@ void swapRows(int* permutations, int row1, int row2) {
     permutations[row2] = temp;
 }
 
-void print(Matrix &m) {
-    int n = m.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            std::cout << m(i, j) << " ";
-        }
-        std::cout << '\n';
-    }
-}
-
-void print(Vector &v) {
-    int n = v.size();
-    for (int i = 0; i < n; i++) {
-        std::cout << v[i] << " ";
-    }
-    std::cout << '\n';
-}
-
-void resetMatrix(Matrix &m) {
-    int size = m.size();
-    Matrix::iterator it = m.begin();
-    for(int i = 0; i < size * size; i++) {
-        *it = 0;
-        it++;
-    }
-}
-
-
-int getMaxNormRowFromRow(Matrix &m, double* norm, int column, int row) {
-    int max = 0;
+int getMaxNormRowFromColumn(Matrix &m, double* norm, int* p, int row) {
+    double max = 0;
     int maxRowNumber = 0;
     for (int i = row; i < m.size(); i++) {
-        double current = std::abs(m(i, column)) / norm[i];
-        if (max < current) {
+        double current = std::abs(m(p[i], row) / norm[p[i]]);
+        if (current > max) {
             max = current;
             maxRowNumber = i;
         }
@@ -79,38 +51,27 @@ void subtractRows(Matrix &m, int row1, int row2, int columnToStart , double mult
     }
 }
 
-void insertOn(Matrix &m, int row, int column, double value) {
-    Matrix::iterator i = m.begin();
-    i += row * m.size() + column;
-    *i = value;
-}
-
-void gaussElimination(Matrix &m, Matrix &l, int* p, double* norm) {
+void gaussElimination(Matrix &m, Vector &l, int* p, double* norm) {
     int size = m.size();
     for (int i = 0; i < size - 1; i++) {
-        int rowToSwap = getMaxNormRowFromRow(m, norm, i, p[i]);
+        int rowToSwap = getMaxNormRowFromColumn(m, norm, p, i);
         swapRows(p, rowToSwap, i);
-        insertOn(l, p[i], i, 1);
-        for (int j = i + 1; j < size; j++) {
+        for (int j = i + 1; j < size; j++){
             double mult = m(p[j], i) / m(p[i], i);
-
-            insertOn(l, p[j], i, mult);
-            subtractRows(m, p[i], p[j], 0, mult);
-            insertOn(m, p[j], i, 0);
+            subtractRows(m, p[i], p[j], i, mult);
+            l[p[j]] -= l[p[i]] * mult;
         }
     }
-    insertOn(l, p[size - 1], size - 1, 1);
 }
-
-Vector* permutate(Vector& v, int* p) {
-    int size = v.size();
-    Vector* result = new Vector(size);
-    Vector::iterator iterator = result->begin();
-    for (int i = 0; i < size; i++) {
-        *iterator = v[p[i]];
+Vector* addVectors(Vector& a, Vector& b) {
+    assert(a.size() == b.size());
+    int n = a.size();
+    Vector* result = new Vector(n);
+    Vector::iterator iterator = (*result).begin();
+    for (int i = 0; i < n; i++) {
+        *iterator = a[i] + b[i];
         iterator++;
     }
-
     return result;
 }
 
@@ -125,10 +86,7 @@ Vector solveEquations(
     double* norm = createNorms(A0);
 
     Matrix* gauss = new Matrix(A0);
-    Matrix* l = new Matrix(size);
-    resetMatrix(*l);
-
-    Vector* correct = new Vector(b);
+    Vector* l = new Vector(b);
 
     int permutations[size];
 
@@ -136,92 +94,29 @@ Vector solveEquations(
         permutations[i] = i;
     }
 
-    std::cout << "vector: \n";
-
-    print(*correct);
-
-    std::cout << "Matrix: \n";
-    print(*gauss);
-
     gaussElimination(*gauss, *l, permutations, norm);
 
-    correct = permutate(*correct, permutations);
-
-    std::cout << '\n';
-
-    std::cout << "After gauss: \n";
-
-    print(*gauss);
-
-    std::cout << "After gauss vector: \n";
-
-    print(*correct);
-
-    std::cout << "permutations: \n";
-    for (int i = 0; i < size; i++) {
-        std::cout << permutations[i] << " ";
-    }
-    std::cout << "\n";
-
-    std::cout << "l: \n";
-
-    print(*l);
-
     Vector* result = new Vector(size);
-    Vector* b1 = new Vector(size);
 
     Vector::iterator iterator = result->end() - 1;
-    Vector::iterator iterator1 = b1->begin();
 
-
-
-
-
-    for (int i = 0; i < size; i++) {
-        *iterator1 = 1;
-        double subtract = 0;
-        for (int j = i - 1; j >= 0; j--) {
-            subtract += (*l)(permutations[i], j) * (*b1)[j];
-        }
-//        std::cout << "iteration: " << i << ": subtract: " << subtract;
-        double res = (*correct)[i] - subtract;
-        *iterator1 = res / (*l)(permutations[i], i);
-//        std::cout << " res: " << res << "/" << (*l)(permutations[i], i) << "=" << *iterator1 << '\n';
-        iterator1++;
-    }
-
-    std::cout << "b: \n";
-    for (int i = 0; i < size; i++) {
-        std::cout << (*b1)[i] << " ";
-    }
-
-    std::cout << "\n";
-
-
+    double temp;
     for (int i = size - 1; i >= 0; i--) {
-        *iterator = 1;
-        double subtract = 0;
-        for (int j = size - 1; j > i; j--) {
-            subtract += (*gauss)(permutations[i], j) * (*result)[j];
-//            std::cout << "subtract: " << (*gauss)(permutations[i], j) << "*" << (*result)[j] << "=" << subtract << '\n';
+        temp = 0;
+        for (int j = i + 1; j < size; j++) {
+            temp += (*gauss)(permutations[i], j) * (*result)[j];
         }
-//        std::cout << "iteration: " << i << ": subtract: " << subtract;
-        double res = (*b1)[i] - subtract;
-        *iterator = res / (*gauss)(permutations[i], i);
-//        std::cout << " res: " << res << "/" << (*gauss)(permutations[i], i) << "=" << *iterator << '\n';
+        *iterator = ((*l)[permutations[i]] - temp) / (*gauss)(permutations[i], i);
         iterator--;
     }
 
-    std::cout << "result: \n";
-    for (int i = 0; i < size; i++) {
-        std::cout << (*result)[i] << " ";
+    Vector r = residual_vector(A0, b, *result);
+
+    while (r.max_norm() >= eps) {
+        Vector e = solveEquations(A0, r, eps);
+        result = addVectors(*result, e);
+        r = residual_vector(A0, b, *result);
     }
-
-    std::cout << "\n";
-
-
 
     return *result;
 }
-
-
